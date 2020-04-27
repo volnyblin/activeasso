36a7ad8e3e1561312dfbaa356e1cad9725fb683a
volnyblin <64414448+volnyblin@users.noreply.github.com>
2020-04-27 15:52:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function closeZoom() {
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos==0) {
     document.getElementById("nav").style.top = "0";
   } else {
     document.getElementById("nav").style.top = "-60px";
